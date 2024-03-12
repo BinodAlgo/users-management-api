bcee3301bfbef8991cf34828e983f7f726a03cde
BinodAlgo <achabinod1112@gmail.com>
2024-03-12 19:43:27 +0545

    add a put route for updating a user by id

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,9 +1,9 @@
 const pool = require('../db/config');
 
 
-const getUsers = (req,res)=>{
-  pool.query('SELECT * FROM users ORDER BY id',(error,results)=>{
-    if(error) throw new Error(error)
+const getUsers = (req, res) => {
+  pool.query('SELECT * FROM users ORDER BY id', (error, results) => {
+    if (error) throw new Error(error)
     res.status(200).json(results.rows)
   })
 
@@ -11,11 +11,11 @@ const getUsers = (req,res)=>{
 }
 
 // GET a user by id 
-const getUserById = (req,res)=>{
-  const {id} = req.params
-  pool.query('SELECT * FROM users WHERE ID =$1',[id],(error,result)=>{
-    if(error){
-      throw error 
+const getUserById = (req, res) => {
+  const { id } = req.params
+  pool.query('SELECT * FROM users WHERE ID =$1', [parseInt(id)], (error, result) => {
+    if (error) {
+      throw error
     }
     res.status(200).json(result.rows);
   })
@@ -23,16 +23,34 @@ const getUserById = (req,res)=>{
 
 
 // Create a new user 
-const createUser = (req,res)=>{
-  const {name,email}  = req.body;
-  pool.query('INSERT INTO users (name,email) VALUES($1, $2) RETURNING *',[name,email],(error,result)=>{
-    if(error) throw error 
+const createUser = (req, res) => {
+  const { name, email } = req.body;
+  pool.query('INSERT INTO users (name,email) VALUES($1, $2) RETURNING *', [name, email], (error, result) => {
+    if (error) throw error
     res.status(201).send(`User added with ID: ${result.rows[0].id}`)
   })
+}
+
+// Update a user by id 
+const updateUser = (req, res) => {
+  const { id } = req.params;
+  const { name, email } = req.body
+
+  pool.query(
+    'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+    [name, email, parseInt(id)],
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      res.status(200).send(`User modified with ID: ${id}`)
+    }
+  )
 }
 
 module.exports = {
   getUsers,
   getUserById,
-  createUser 
+  createUser,
+  updateUser
 }
--- routes/route.users.js
+++ routes/route.users.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const {getUsers,getUserById, createUser} = require('../controllers/users.controller');
+const { getUsers, getUserById, createUser, updateUser } = require('../controllers/users.controller');
 
 // Get all users 
-router.get("/",getUsers);
+router.get("/", getUsers);
 
-router.get("/:id",getUserById);
+router.get("/:id", getUserById);
 
-router.post("/",createUser);
+router.post("/", createUser);
+
+router.put("/:id", updateUser);
 
 module.exports = router;  
