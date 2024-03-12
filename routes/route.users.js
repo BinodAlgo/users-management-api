11879c2017a904e6c3cef18b9e6bbeeaafa16f46
BinodAlgo <achabinod1112@gmail.com>
2024-03-12 18:57:15 +0545

    add database connection pool

--- routes/route.users.js
+++ routes/route.users.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const dbConfig = require('../db/config')
 require('dotenv').config();
 
+// Database Connection 
+const Pool = require('pg').Pool;
+const pool = new Pool(
+dbConfig.config
+)
 router.get("/",(req,res)=>{
   res.json({users:""});
 })
