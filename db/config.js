f9f3abc23c70905eb7faa039c91c1055a20d0358
BinodAlgo <achabinod1112@gmail.com>
2024-03-12 19:04:18 +0545

    moved database connection logic to config.js from route.users.js

--- db/config.js
+++ db/config.js
@@ -1,9 +1,14 @@
-module.exports = {
-  config:{
+// Database Connection 
+const Pool = require('pg').Pool;
+const pool = new Pool(
+{
     user: process.env.USER, 
     password:process.env.password, 
     host: process.env.HOST, 
     port: process.env.DB_PORT, 
     database: process.env.DATABASE
   }
-}
+)
+
+
+module.exports = pool 
