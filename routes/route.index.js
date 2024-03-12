e9ea9c993cc1af2931f8c0a61180c79648830af4
BinodAlgo <achabinod1112@gmail.com>
2024-03-12 18:40:22 +0545

    fix router.use() to use users route

--- routes/route.index.js
+++ routes/route.index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const userRouter = require('./route.users');
 const router = express.Router();
 
-router.use('/users', userRouter);
+router.use(userRouter);
 
 module.exports = router;
