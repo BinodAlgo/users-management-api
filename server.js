bf322f9db6fdb43bdab822d955836ac1f65758ca
BinodAlgo <achabinod1112@gmail.com>
2024-03-12 18:20:53 +0545

    Add express app() and start the app

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 // Declaration of variables 
 const express = require('express');
+const app = express();
 const cors = require('cors');
 const PORT = process.env.PORT || 3000;
-
-
 
 // App Configuration 
 app.use(express.json());
@@ -12,3 +11,12 @@ app.use(cors());
 
 // Routes 
 app.use(require('./routes/route.index'));
+
+
+
+
+
+
+
+// Start the server 
+app.listen(PORT);
