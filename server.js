d7a6a36fa2c718f411b9b42e47556090200d795b
BinodAlgo <achabinod1112@gmail.com>
2024-03-12 19:07:34 +0545

    add dotenv config across all routes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cors());
-
+require('dotenv').config();
 // Routes 
 app.use(require('./routes/route.index'));
 
