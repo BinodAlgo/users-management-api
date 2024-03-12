aae0716b2defc210c35edf6901bd54507587d422
BinodAlgo <achabinod1112@gmail.com>
2024-03-12 18:46:46 +0545

    add database config

---
+++ db/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  config:{
+    user: process.env.USER, 
+    password:process.env.password, 
+    host: process.env.HOST, 
+    port: process.env.DB_PORT, 
+    database: process.env.DATABASE
+  }
+}
